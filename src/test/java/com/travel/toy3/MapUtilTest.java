b2e0ab39e1699ca5abcfd5ded4b837ad130218e2
ygg <rnrn4728@gmail.com>
2024-02-19 16:25:55 +0900

    Rename : 클래스명 수정(m -> M)

--- src/test/java/com/travel/toy3/MapUtilTest.java
+++ src/test/java/com/travel/toy3/MapUtilTest.java
@@ -8,10 +8,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.io.IOException;
 
 @SpringBootTest
-public class mapUtilTest {
+public class MapUtilTest {
     @Test
     void mapTest() throws IOException {
-        GoogleMap a = Geocoding.getGeoInfo("삼성동");
+        GoogleMap a = Geocoding.getGeoInfo("서울특별시 마포구 와우산로26길 9");
         System.out.println("========================================================================");
         System.out.println(a);
         System.out.println("========================================================================");
