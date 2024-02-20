69a22cdcb0998faae38ccb7abc9c85a9212b5480
박하은 <deveun23@gmail.com>
2024-02-21 01:11:01 +0900

    Feat : Member -> MemberDTO 변환 메서드 구현

--- src/main/java/com/travel/toy3/domain/member/dto/MemberDTO.java
+++ src/main/java/com/travel/toy3/domain/member/dto/MemberDTO.java
@@ -1,11 +1,10 @@
 package com.travel.toy3.domain.member.dto;
 
+import com.travel.toy3.domain.member.entity.Member;
 import com.travel.toy3.domain.trip.entity.Trip;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
@@ -13,13 +12,22 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Entity
+@Builder
 public class MemberDTO {
     private String username;
     private String password;
     private String uname;
     // 본인이 작성한 여행 정보 리스트
     private List<Trip> trips;
+
+    public static MemberDTO fromEntity(Member member) {
+        return MemberDTO.builder()
+                .username(member.getUsername())
+                .password(member.getPassword())
+                .uname(member.getUname())
+                .build();
+    }
 }
