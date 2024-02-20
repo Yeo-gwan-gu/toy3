f609f2afb171396fd9513c175ed6f0702b87c00e
박하은 <deveun23@gmail.com>
2024-02-21 01:13:54 +0900

    Feat : Member 전체 조회 기능 구현
    Spring Security를 붙이기 전, 다른 도메인 테스트에서 사용할 사용자 기본 틀 구현

--- src/main/java/com/travel/toy3/domain/member/service/MemberService.java
+++ src/main/java/com/travel/toy3/domain/member/service/MemberService.java
@@ -1,4 +1,18 @@
 package com.travel.toy3.domain.member.service;
 
+import com.travel.toy3.domain.member.entity.Member;
+import com.travel.toy3.domain.member.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class MemberService {
+    @Autowired
+    private MemberRepository memberRepository;
+
+    public List<Member> getAllMembers() {
+        return memberRepository.findAll();
+    }
 }
