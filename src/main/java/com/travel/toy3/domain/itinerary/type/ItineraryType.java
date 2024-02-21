5978930d1499049d083de833c9c1ebe3da31709c
박하은 <deveun23@gmail.com>
2024-02-21 14:13:38 +0900

    Modify : 여정 타입 Enum 수정

--- src/main/java/com/travel/toy3/domain/itinerary/type/ItineraryType.java
+++ src/main/java/com/travel/toy3/domain/itinerary/type/ItineraryType.java
@@ -6,10 +6,9 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ItineraryType {
-    MOVING(1, "이동"),
-    ACCOMMODATION(2, "숙박"),
-    STAY(3, "체류");
+    MOVING("이동"),
+    ACCOMMODATION("숙박"),
+    STAY("체류");
 
-    private final Integer code;
     private final String korean;
 }
