22f826e8aeb2c2cf58b06eaf21c60c802b546782
ygg <rnrn4728@gmail.com>
2024-02-21 15:10:20 +0900

    Feat : @Setter 추가

--- src/main/java/com/travel/toy3/domain/member/entity/Member.java
+++ src/main/java/com/travel/toy3/domain/member/entity/Member.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.Email;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
@@ -13,6 +14,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
