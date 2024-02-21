5ab23b1576ff5c1f5e141496e7f7b8694fd7b5c5
박하은 <deveun23@gmail.com>
2024-02-21 12:35:57 +0900

    Feat : 체류 Entity 생성

---
+++ src/main/java/com/travel/toy3/domain/itinerary/entity/Stay.java
@@ -0,0 +1,36 @@
+package com.travel.toy3.domain.itinerary.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Stay {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; // 체류 id (PK)
+
+    @ManyToOne
+    @JoinColumn(name = "itineraryId")
+    private Itinerary itinerary; // 여정 테이블과 관계 설정 -> 여정 id (FK)
+
+    private String stayPlaceName; // 장소명
+    private Double stayPlaceLatitude; // 장소 위도
+    private Double stayPlaceLongitude; // 장소 경도
+    private LocalDateTime arrivalDatetime; // 도착 일시
+    private LocalDateTime departureDatetime; // 출발 일시
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
