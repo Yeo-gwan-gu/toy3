28ca3f3c3cf298c63ef7ce4ce30d1886c89ad5f2
ygg <rnrn4728@gmail.com>
2024-02-22 10:56:05 +0900

    Modify : 예외처리 수정

--- src/main/java/com/travel/toy3/domain/member/service/CustomUserDetailsService.java
+++ src/main/java/com/travel/toy3/domain/member/service/CustomUserDetailsService.java
@@ -8,10 +8,9 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
 import java.util.Optional;
 
-import static com.travel.toy3.exception.CustomErrorCode.INCORRECT_USERNAME_FORMAT;
+import static com.travel.toy3.exception.CustomErrorCode.INVALID_USERNAME;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
@@ -25,7 +24,7 @@ public class CustomUserDetailsService implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Optional<Member> optional = memberRepository.findByUsername(username);
         if (!optional.isPresent()) {
-            throw  new CustomException(INCORRECT_USERNAME_FORMAT);
+            throw  new CustomException(INVALID_USERNAME);
         }
         Member member=optional.get();
         return new CustomMember(member);
