3051c91090560c89367e59478103b069f3485fdf
박하은 <deveun23@gmail.com>
2024-02-23 18:27:50 +0900

    Feat : 숙박 등록 기능 구현, 체류 등록 기능 구현
    구글맵 연동을 제외한 여정 등록 기능 구현 완료

--- src/main/java/com/travel/toy3/domain/itinerary/controller/ItineraryRestController.java
+++ src/main/java/com/travel/toy3/domain/itinerary/controller/ItineraryRestController.java
@@ -1,6 +1,8 @@
 package com.travel.toy3.domain.itinerary.controller;
 
+import com.travel.toy3.domain.itinerary.dto.CreateAccommodation;
 import com.travel.toy3.domain.itinerary.dto.CreateMoving;
+import com.travel.toy3.domain.itinerary.dto.CreateStay;
 import com.travel.toy3.domain.itinerary.service.ItineraryService;
 import com.travel.toy3.util.ApiResponse;
 import jakarta.validation.Valid;
@@ -20,11 +22,42 @@ public class ItineraryRestController {
             @PathVariable Long tripId,
             @Valid @RequestBody final CreateMoving.Request request
     ) {
-
         ApiResponse<CreateMoving.Response> response = ApiResponse.<CreateMoving.Response>builder()
                 .resultCode(HttpStatus.OK.value())
                 .resultMessage(HttpStatus.OK.getReasonPhrase())
                 .data(itineraryService.addMoving(tripId, request))
+                .build();
+
+        return ResponseEntity
+                .status(HttpStatus.OK.value())
+                .body(response);
+    }
+
+    @PostMapping("/{tripId}/accommodation")
+    public ResponseEntity<ApiResponse<CreateAccommodation.Response>> addAccommodation(
+            @PathVariable Long tripId,
+            @Valid @RequestBody final CreateAccommodation.Request request
+    ) {
+        ApiResponse<CreateAccommodation.Response> response = ApiResponse.<CreateAccommodation.Response>builder()
+                .resultCode(HttpStatus.OK.value())
+                .resultMessage(HttpStatus.OK.getReasonPhrase())
+                .data(itineraryService.addAccommodation(tripId, request))
+                .build();
+
+        return ResponseEntity
+                .status(HttpStatus.OK.value())
+                .body(response);
+    }
+
+    @PostMapping("/{tripId}/stay")
+    public ResponseEntity<ApiResponse<CreateStay.Response>> addStay(
+            @PathVariable Long tripId,
+            @Valid @RequestBody final CreateStay.Request request
+    ) {
+        ApiResponse<CreateStay.Response> response = ApiResponse.<CreateStay.Response>builder()
+                .resultCode(HttpStatus.OK.value())
+                .resultMessage(HttpStatus.OK.getReasonPhrase())
+                .data(itineraryService.addStay(tripId, request))
                 .build();
 
         return ResponseEntity
