3df792f3d3373a6cb11e7ee8eac5e9ae374480cf
박하은 <deveun23@gmail.com>
2024-02-23 10:18:02 +0900

    Modify : 여정 관련 Entity에 @Builder 추가

--- src/main/java/com/travel/toy3/domain/itinerary/entity/Stay.java
+++ src/main/java/com/travel/toy3/domain/itinerary/entity/Stay.java
@@ -2,6 +2,7 @@ package com.travel.toy3.domain.itinerary.entity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
@@ -13,6 +14,7 @@ import java.time.LocalDateTime;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 @Entity
 @EntityListeners(AuditingEntityListener.class)
 public class Stay {
