8c81228d969885c9b4ff468cbb0a29d2517a57ad
박하은 <deveun23@gmail.com>
2024-02-23 17:07:34 +0900

    Add : 여정 등록 DTO 생성

---
+++ src/main/java/com/travel/toy3/domain/itinerary/dto/ItineraryDTO.java
@@ -0,0 +1,41 @@
+package com.travel.toy3.domain.itinerary.dto;
+
+import com.travel.toy3.domain.itinerary.entity.Itinerary;
+import com.travel.toy3.domain.itinerary.entity.Moving;
+import com.travel.toy3.domain.itinerary.type.ItineraryType;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+public class ItineraryDTO {
+    private Long tripId;
+    private Long itineraryId;
+    private ItineraryType itineraryType;
+    private String itineraryName;
+    private MovingDTO moving;
+//    private AccommodationDTO accommodationDTO;
+//    private StayDTO stayDTO;
+
+    public static ItineraryDTO fromEntity(@NonNull Itinerary itinerary, @NonNull Moving moving) {
+
+        return ItineraryDTO.builder()
+                .tripId(itinerary.getTrip().getId())
+                .itineraryId(itinerary.getId())
+                .itineraryType(itinerary.getItineraryType())
+                .itineraryName(itinerary.getItineraryName())
+                .moving(MovingDTO.builder()
+                        .vehicle(moving.getVehicle())
+                        .departurePlace(moving.getDeparturePlace())
+                        .destinationPlace(moving.getDestinationPlace())
+//                        .departurePlaceAddress()
+//                        .destinationPlaceAddress()
+                        .departureDatetime(moving.getDepartureDatetime())
+                        .arrivalDatetime(moving.getArrivalDatetime())
+                        .build())
+                .build();
+    }
+}
