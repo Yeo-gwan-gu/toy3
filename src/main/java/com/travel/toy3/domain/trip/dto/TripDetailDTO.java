cfaa675b4c8deb04f7cfa0470dfaacab6d5b8f33
aelimited <cloim09@gmail.com>
2024-02-24 12:37:29 +0900

    Modify : Itinerary 여행 상세정보에 출력 추가

--- src/main/java/com/travel/toy3/domain/trip/dto/TripDetailDTO.java
+++ src/main/java/com/travel/toy3/domain/trip/dto/TripDetailDTO.java
@@ -1,5 +1,6 @@
 package com.travel.toy3.domain.trip.dto;
 
+import com.travel.toy3.domain.itinerary.dto.CreateItinerary;
 import com.travel.toy3.domain.itinerary.entity.Itinerary;
 import com.travel.toy3.domain.trip.entity.Comment;
 import com.travel.toy3.domain.trip.entity.Trip;
@@ -25,32 +26,35 @@ public class TripDetailDTO {
     private LocalDate tripArrivalDate;
     private String tripDestination;
     private Boolean isDomestic;
-    private List<Itinerary> itineraries;
-   // @Formula("(select count * from )")
+    private List<CreateItinerary.Response> itineraries;
     private Integer likeCount;
     private Integer commentCount;
     private List<CommentDTO.Response> comments;
 
     public static TripDetailDTO fromEntity(
             @NotNull Trip trip,
+            @NotNull List<Itinerary> itineraries,
             @NotNull Integer likeCount,
             @NotNull List<Comment> comments,
             @NotNull Integer commentCount
-            // @NotNull Trip trip, Member member, Itinerary itinerary, Like like, Comment comment
     ) {
+        List<CreateItinerary.Response> itineraryList = itineraries.stream()
+                .map(CreateItinerary.Response::fromEntity)
+                .collect(Collectors.toList());
+
+
         List<CommentDTO.Response> commentList = comments.stream()
                 .map(CommentDTO.Response::fromEntity)
                 .collect(Collectors.toList());
 
         return TripDetailDTO.builder()
                 .id(trip.getId())
-//                .username(member.getUsername())
                 .tripName(trip.getTripName())
                 .tripDepartureDate(trip.getTripDepartureDate())
                 .tripArrivalDate(trip.getTripArrivalDate())
                 .tripDestination(trip.getTripDestination())
                 .isDomestic(trip.getIsDomestic())
-//                .itineraries(itinerary.getItinerary())
+                .itineraries(itineraryList)
                 .likeCount(likeCount)
                 .commentCount(commentCount)
                 .comments(commentList)
