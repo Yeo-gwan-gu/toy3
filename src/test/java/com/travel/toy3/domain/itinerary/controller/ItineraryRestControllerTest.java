09360aaee9f565ec8f3657f133b81827ee7a5a91
박하은 <deveun23@gmail.com>
2024-02-25 18:09:28 +0900

    Add : ItineraryRestControllerTest 생성

---
+++ src/test/java/com/travel/toy3/domain/itinerary/controller/ItineraryRestControllerTest.java
@@ -0,0 +1,12 @@
+package com.travel.toy3.domain.itinerary.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ItineraryRestControllerTest {
+    @Test
+    void getAllItineraries() {
+
+    }
+}
